7d9574d59811e3cae5e683cf1ac456c44e209f7e
asder707 <abn.sebick@gmail.com>
2018-06-24 21:58:40 -0400

    se arregla environment produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBRGGMHWZeZecIBoqDrM5M_Ig8bnlI1X7w",
+    authDomain: "nerudaapp.firebaseapp.com",
+    databaseURL: "https://nerudaapp.firebaseio.com",
+    projectId: "nerudaapp",
+    storageBucket: "nerudaapp.appspot.com",
+    messagingSenderId: "263413827853"
+  }
 };
